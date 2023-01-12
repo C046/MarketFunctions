9680aeeb9d84bf9f0520189ed0ae01f481077e6b
C046 <79424595+C046@users.noreply.github.com>
2023-01-11 19:49:03 -0800

    Add files via upload

---
+++ MarketFunctions.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 11 17:53:04 2023
+
+@author: hadaw
+"""
+from itertools import cycle
+import numpy as np
+
+
+# GROWTH RATE
+def _gr(InitialEPSValue,FinalEPSValue):
+    # Define A Generator To Speed Up The Code
+    def _GR(InitialEPSValue, FinalEPSValue):
+        while True:
+            # Subtract FinalEPSValue With InitialEPSValue then divide it by the InitialEPSValue
+            yield [((np.subtract(fv,iv))/iv) for fv,iv in zip(FinalEPSValue,cycle(InitialEPSValue))]
+      
+    # Automate The Generator So That You Can Write Less Code
+    for i in _GR(InitialEPSValue, FinalEPSValue):
+        return i
+    
+    
+# PE RATIO
+def _pe(MarketValuePerShare,EarningsPerShare):
+    # Define A Generator To Speed Up The Code
+    def _PE(MarketValuePerShare, EarningsPerShare):
+        while True:
+            # Divide MarketValuePerShare With EarningsPerShare
+            yield [np.divide(mvp,eps) for mvp,eps in zip(MarketValuePerShare,cycle(EarningsPerShare))]
+      
+    # Automate The Generator So That You Can Write Less Code
+    for i in _PE(MarketValuePerShare,EarningsPerShare):
+        return i
+
+
+# R-Expected earnings growth rate.
+def _r(PE,ProjectedGrowthInEarnings):
+    # Define A Generator To Speed Up The Code
+    def _R(PE,ProjectedGrowthInEarnings):
+        while True:
+            # Divide PE with ProjectedGrowthInEarnings
+            yield [np.divide(pe,pgs) for pe,pgs in zip(PE,cycle(ProjectedGrowthInEarnings))]
+      
+    # Automate The Generator So That You Can Write Less Code
+    for i in _R(PE,ProjectedGrowthInEarnings):
+        return i
+
+
+# ESTIMATED PROFIT PER SHARE.
+def _eps(NetIncome,PreferredDividends,AverageOutstandingCommonShares):
+    # Define A Generator To Speed Up The Code
+    def _EPS(NetIncome,PreferredDividends,AverageOutstandingCommonShares):
+        while True:
+            # Subtract NetIncome With Preferred Dividends Then Divide By the AverageOutstandingShares
+            yield [((np.subtract(ni,_pd))/aocs) for ni,_pd,aocs in zip(NetIncome,PreferredDividends,cycle(AverageOutstandingCommonShares))]
+      
+    # Automate The Generator So That You Can Write Less Code
+    for i in _EPS(NetIncome,PreferredDividends,AverageOutstandingCommonShares):
+        return i
+
+
+# INTRINSIC VALUE FUNCTION 
+def _iv(eps,r,pe):
+    # Define A Generator To Speed Up The Code
+    def _INT(eps,r,pe):
+        while True:
+            # Add 1 to R Then Multiply EPS With R, Multiply This Value With PE Value
+            yield [np.multiply(np.multiply(EPS,(1+R)),PE) for EPS,R,PE in zip(eps,r,cycle(pe))]
+    
+    # Automate The Generator So That You Can Write Less Code
+    for i in _INT(eps,r,pe):
+        return np.average(i)
+    
+        
+        
+        
+  
+   
+        
+
+NetIncome,PreferredDividends,AverageOutstandingCommonShares = [4,5,6,1,2,3],[3,2,1,5,4,12],[4,5,6,1,2,3]
+EPS = _eps(NetIncome,PreferredDividends,AverageOutstandingCommonShares)
+MarketValuePerShare =[4,5,6,1,2,3]
+PE = _pe(MarketValuePerShare,EPS)
+ProjectedGrowthInEarnings = [4,5,6,1,2,3]
+
+R = _r(PE,ProjectedGrowthInEarnings)
+
+
+Intrinsic_value = _iv(EPS,R,PE)
