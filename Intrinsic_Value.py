8daf2cd9e1f4906af28b5c8e275aaff624ab0931
C046 <79424595+C046@users.noreply.github.com>
2023-01-15 22:46:06 -0800

    Update Intrinsic_Value.py

--- Intrinsic_Value.py
+++ Intrinsic_Value.py
@@ -4,7 +4,6 @@ Created on Fri Jan 13 18:32:12 2023
 
 @author: hadaw
 """
-""" This function is purely speculative, and is my own math that uses an intrinsic value function to calculate the value of a company. """
 import numpy as np
 import requests
 from MarketFunctions import _iv, _eps, _r, _gr,_pe
@@ -25,6 +24,15 @@ print("\n Welcome to my intrinsic value application for when I am at work. \n Yo
 print("______________________________________________________________________________\n______________________________________________________________________________")
 
 symbol = _c(str(input("\n Please type in a ticker symbol: ")))
+sureness = float(input("\n On a scale 1-10, how sure are you about the stock going up: "))/10
+
+# for i in sureness:
+#     if i == "y" or i == "Y":
+#         sureness = "Yes"
+    
+#     if i == "n" or i == "N":
+#         sureness = "No"
+        
 api_key = "QNI1JU2CWCRNHF9T"
 
 
@@ -128,39 +136,71 @@ low = "3. low"
 MarketValuePerShare = r.json()[tuple(r.json())[1]]
 MarketValuePerShare_low = [float(MarketValuePerShare[e][low]) for i,e in enumerate(MarketValuePerShare)]
 MarketValuePerShare_high = [float(MarketValuePerShare[e][high]) for i,e in enumerate(MarketValuePerShare)]
-NetIncome = np.array([float(i) for i in NetIncome])
-P_Div = np.array([float(i) for i in P_Div])
-commonStockSharesOutstanding = np.array([float(i) for i in commonStockSharesOutstanding])
+MarketValuePerShare = MarketValuePerShare_low+MarketValuePerShare_high
+
+NetIncome = [float(i) for i in NetIncome]
+P_Div = [float(i) for i in P_Div]
+commonStockSharesOutstanding = [float(i) for i in commonStockSharesOutstanding]
 
 
 eps = _eps(NetIncome,P_Div,commonStockSharesOutstanding)
-pe_low = _pe(MarketValuePerShare_low,eps)
-pe_high = _pe(MarketValuePerShare_high,eps)
+pe = _pe(MarketValuePerShare, eps)
+
+# # pe_low = _pe(MarketValuePerShare_low,eps)
+# # pe_high = _pe(MarketValuePerShare_high,eps)
+
+# PE = np.average([np.average(pe_low),np.average(pe_high)])
+
 
 ProjectedGrowthInEarnings = _gr(eps)
-r_low = _r(pe_low,ProjectedGrowthInEarnings)
-r_high = _r(pe_high,ProjectedGrowthInEarnings)
+r = _r(pe,ProjectedGrowthInEarnings)
+
+# r+ProjectedGrowthInEarnings+pe+eps+commonStockSharesOutstanding+P_Div+NetIncome+MarketValuePerShare
+#r_high = _r(pe_high,ProjectedGrowthInEarnings)
+
+
 
 # Automate your brain.
 if eps == 0.0:
     eps = -0.000000000000000000000000000000000000000000000000000000000000000001
+# log_this = np.average([MarketValuePerShare+ NetIncome+ P_Div+ commonStockSharesOutstanding+ eps+pe+ProjectedGrowthInEarnings+r])
+# if log_this <=0:
+#     log_this = log_this*-1
+# c = math.sqrt(math.log(log_this)**2)
+
+
+if (_iv(eps,r,pe) - np.average(eps)) <=0:
+    x = (_iv(eps,r,pe) - np.average(eps))*-1
+else:
+    x = _iv(eps,r,pe) - np.average(eps)
     
-Intrinsic_Value = (_iv(eps,r_low,pe_low)+_iv(eps,r_high,pe_high))/2
+top = math.log(math.sqrt(x))**2#(np.average(np.average(ProjectedGrowthInEarnings)))
+bottom = math.log((np.average(commonStockSharesOutstanding) - np.average(eps))**np.average(ProjectedGrowthInEarnings))**2
 
-
-if Intrinsic_Value <=0:
-    Intrinsic_Value = Intrinsic_Value*-1
+if top/(math.sqrt(math.log(bottom))**2) <= 95:
+    res = (top/(math.sqrt(math.log(bottom))**2))**2
+    sure = res*sureness
+    if sureness >=0.5:
+        res = res+sure+sure
+    if sureness <=0.3:
+        res = res-sure
     
-if Intrinsic_Value/np.array(eps).mean() <=0:
-    Intrinsic_Value = Intrinsic_Value/np.array(eps).mean()*-1
+    print(f"\n Your intrinsic value share price for {symbol} is: {res}")
+if top/(math.sqrt(math.log(bottom))**2) >= 95:
+    res = top/(math.sqrt(math.log(bottom))**2)
+    sure = res*sureness
+    if sureness >=0.5:
+        res = res+sure+sure
+    if sureness <=0.3:
+        res = res-sure
+        
+    
+    print(f"\n Your intrinsic value share price for {symbol} is: {res}")
+# if top/bottom <= 999:
+#     print(f"\n Your intrinsic value share price for {symbol} is: {top/bottom}")
 
-if Intrinsic_Value/np.array(eps).mean() >= 0:
-    Intrinsic_Value = Intrinsic_Value/np.array(eps).mean()
-
-
-Intrinsic_Value = (math.sqrt(Intrinsic_Value))/2
-
-
-print(f"\n Your intrinsic value share price for {symbol} is: {Intrinsic_Value}")
+# if sureness == "No":
+#     print(f"\n Your intrinsic value share price for {symbol} is: {(top/bottom)*100}")
+    
 
 #########################################################################
